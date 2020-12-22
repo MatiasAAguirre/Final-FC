6f4c8639d1d148c79d6548f2709e6637f17eac65
MatiasAAguirre <aguirrematiasa@live.com.ar>
2020-12-22 16:52:05 -0300

    Agrego a cuenta_conex.h lo que creo que me va a servir

--- cuenta_conex.h
+++ cuenta_conex.h
@@ -19,3 +19,26 @@ int imprimir_matriz(int *matriz, int f, int c) {
 
    return 0;
  }
+
+ int ver_repe(int *vec, int l, int Ka) {
+   int i, j, k, c;
+
+   for (i=0;i<l;i++) {
+     c = 0;
+     for (j=0;j<2*Ka;j++) {
+       printf("%d ", *(vec+i*2*Ka+j));
+       for (k=0; k<j; k++) {
+         if (*(vec+i*2*Ka+j)==*(vec+i*2*Ka+k)) {
+           c++;
+         }
+       }
+     }
+     printf("\n");
+     if (c!=0) {
+         printf("Hay %d repeticiones para la fila %d.\n", c, i);
+     }
+     printf("\n");
+   }
+
+   return 0;
+ }
